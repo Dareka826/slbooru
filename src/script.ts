1bb592237986a1184648137db4c9a9b7d155eef1
Rin Takanashi <dareka826@gmail.com>
2021-03-11 15:53:11 +0100

    Disable buttons based on page number

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,9 @@
 // Get elem by id function
 function gid(id:string):any { return document.getElementById(id); }
 
+// Variables
+const buttonDisabledColor = "#888";
+
 // Get arguments from GET request
 let parameters = new URLSearchParams(window.location.search);
 let query = parameters.get("q") || "";
@@ -11,6 +14,21 @@ let search_button:HTMLButtonElement = gid("search_button");
 
 // Set the search field to the query
 search_input.value = query;
+
+// Disable navigation buttons based on page id
+let page_info = gid("page-indicator").innerText.split('/');
+if(page_info[0] == page_info[1]) {
+	let btn = gid("next-page-btn");
+	btn.disabled = true;
+	btn.style.borderColor = buttonDisabledColor;
+	btn.style.color = buttonDisabledColor;
+}
+if(page_info[0] == "1") {
+	let btn = gid("prev-page-btn");
+	btn.disabled = true;
+	btn.style.borderColor = buttonDisabledColor;
+	btn.style.color = buttonDisabledColor;
+}
 
 // If in single image mode
 if(parameters.get("m") == "i") {
