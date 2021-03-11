63161476507bce926e5c000f09638082a0bfcd37
Rin Takanashi <dareka826@gmail.com>
2021-03-11 11:22:09 +0100

    Server: - Change bind to listen locally only - Server now works mostly (the oly thing left is potential tag display)

--- server.ts
+++ server.ts
@@ -13,18 +13,141 @@ const extensionsMIME = {
 	".mkv" : "video/mkv"
 };
 
-const pageTemplate = fs.readFileSync("index.html", "utf8");
-const picsPerPage = 3;
+const pageTemplate:string = fs.readFileSync("index.html", "utf8");
+const picsPerPage = 42;
 const metadata = [];
 
-function genPic(id:number):string {
-	let e:string = "<div class=\"image-small-container\"><img src=\"";
-	e += "img/" + metadata[id].file;
-	e += "\" class=\"image-small\"></img></div>";
+function genPage(urlparams:URLSearchParams):string {
+	let data = pageTemplate;
+	let page_variant = urlparams.get("m") || "p";
+
+	if(page_variant == "p") {
+		// Posts view
+		const query = urlparams.get("q") || "";
+		const page_id = Number(urlparams.get("p") || "0");
+
+		let candidates:Array<number> = matchImagesToQuery(query);
+
+		// No results for query
+		if(candidates.length == 0) {
+			let picDOM = '<div class="info-notfound">No results</div>';
+			return data.replace("<!--TAGS-->", picDOM);
+		}
+
+		// Decide which results to show based on page id
+		const lowerID = page_id * picsPerPage;
+		const higherID = (page_id + 1) * picsPerPage - 1;
+
+		if(lowerID > candidates.length) {
+			// Page number out of range
+			let picDOM = '<div class="info-notfound">Page index out of range!</div>';
+			return data.replace("<!--TAGS-->", picDOM);
+		}
+
+		let picDOM = "";
+		for(let id = lowerID; id <= higherID && id < candidates.length; id++)
+			// Iterate over ids on page
+			picDOM += genPic(candidates[id], "small");
+
+		data = data.replace("<!--PICS-->", picDOM);
+	} else if(page_variant == "i") {
+		// Single image view
+		const image_id = Number(urlparams.get("i") || "0");
+
+		// Create the picture element
+		let picDOM = genPic(image_id, "large");
+		// Add a link to get the image directly
+		picDOM += '<div style="padding-left: 5px;"><a href="img/';
+		picDOM += metadata[image_id].file + '">Original Image</a></div>';
+		// Insert the generated data
+		data = data.replace("<!--PICS-->", picDOM);
+		
+		// Generate the tag view
+		let tagDOM = genTags("", true, image_id);
+		data = data.replace("<!--TAGS-->", tagDOM);
+	}
+
+	return data;
+}
+
+function matchImagesToQuery(query:string):Array<number> {
+	let qualified:Array<number> = [];
+	let rules = query.split(" ");
+
+	// Initialize with all
+	for(let i = 0; i < metadata.length; i++)
+		qualified.push(i);
+
+	for(let rule of rules) {
+		if(rule[0] == '-') {
+			// Exclude tag
+			let tag = rule.substr(1, rule.length);
+
+			// Remove ids that have the tag
+			for(let i = qualified.length-1; i >= 0; i--)
+				if(metadata[qualified[i]].tags.includes(tag))
+					qualified.splice(i, 1);
+		} else if(rule != "") {
+			// Match tag
+			let tag = rule;
+			// Remove ids that don't have the tag
+			for(let i = qualified.length-1; i >= 0; i--)
+				if(!metadata[qualified[i]].tags.includes(tag))
+					qualified.splice(i, 1);
+		}
+	}
+
+	return qualified;
+}
+
+function genTags(query:string, single_image:boolean, image_id:number = -1):string {
+	let tagDOM = "";
+	if(single_image) {
+		let tags = metadata[image_id].tags;
+
+		// For every tag of the image
+		for(let tag of tags) {
+			// Count the number of occurences of the tag
+			let tagCount = 0;
+			for(let i = 0; i < metadata.length; i++)
+				if(metadata[i].tags.includes(tag))
+					tagCount++;
+
+			// Append the created tag element to the tag sidebar
+			tagDOM += createTagElem(tag, tagCount, false);
+		}
+	}
+	return tagDOM;
+}
+
+function createTagElem(tag:string, count:number, add_exclude_option:boolean) {
+	let tagDOM = '<div class="tagitem">'
+	tagDOM += '<a href="javascript:;" onclick="changeQuery(';
+	tagDOM += "'" + tag + "'" + ', ' + "'replace'" + ')">' + tag;
+	tagDOM += '</a>&nbsp;<span class="count">';
+	tagDOM += count;
+	tagDOM += '</span></div>';
+	return tagDOM;
+}
+
+type ImageVariant = "small" | "large";
+function genPic(id:number, variant:ImageVariant):string {
+	let e:string = '<div class="';
+
+	e += (variant == "small") ? "image-small-container" : "image-big-container";
+
+	e += '"><img src="';
+	e += 'img/' + metadata[id].file;
+	e += '" class="';
+
+	e += (variant == "small") ? "image-small" : "image-large";
+
+	e += '"></img></div>';
 	return e;
 }
 
-function parseMetadata() {
+// Reads the metadata json files and puts them into the metadata variable
+function pullInMetadata() {
 	const metadataFiles = fs.readdirSync("metadata");
 	metadataFiles.sort();
 	let x = 0;
@@ -56,27 +179,21 @@ const server = http.createServer((req:any, res:any) => {
 
 	// Get url GET parameters
 	let urlparams = new URLSearchParams(req.url.replace(/^\//, ""));
-	const query = urlparams.get("q");
-	const page = urlparams.get("p") || "0";
 
 	// Set HTTP header
 	res.statusCode = 200;
 	res.setHeader("Content-Type", "text/html");
 
 	// Generate the page
-	let picDOM = "";
-	for(let i = 0; i < 9; i++)
-		picDOM += genPic(i);
-	let data = pageTemplate.replace("<!--PICS-->", picDOM);
-	res.write(data);
+	res.write(genPage(urlparams));
 
 	res.end();
 });
 
 // Prepare metadata
-parseMetadata();
+pullInMetadata();
 
 // Run the server
-server.listen("8000", "0.0.0.0", () => {
+server.listen(8000, '127.0.0.1', () => {
 	console.log("Server running...");
 });
