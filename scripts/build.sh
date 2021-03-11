6bc6cf313c762d1aadec6e3fd37f17e5ea1ab958
Rin Takanashi <dareka826@gmail.com>
2021-03-11 20:31:11 +0100

    build.sh now doesn't execute added a script to build and run

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,4 @@
 rm src/script.js server.js
 
 tsc --outFile src/script.js src/script.ts && \
-tsc server.ts && \
-node server.js
+tsc server.ts
---
+++ scripts/build_test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./scripts/build.sh && node server.js
