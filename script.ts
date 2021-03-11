7b81f2864319f3dc771616fa7658f0db62432c61
Rin Takanashi <dareka826@gmail.com>
2021-03-11 11:19:27 +0100

    Enter on input field also searches

--- script.ts
+++ script.ts
@@ -11,10 +11,24 @@ let search_button = gid("search_button");
 
 search_input.value = query;
 
-search_button.addEventListener("click", function() {
+function search_execute() {
 	let parameters = new URLSearchParams();
 	parameters.set("q", search_input.value);
 	parameters.set("p", page);
 	window.location.replace("/?" + parameters.toString());
+}
+
+search_button.addEventListener("click", search_execute);
+search_input.addEventListener("keypress", function(e) {
+	if(e.key == "Enter")
+		search_execute();
 });
 
+type TagChangeMode = "add" | "exclude" | "replace";
+function changeQuery(tag:string, mode:TagChangeMode) {
+	if(mode == "add")          query += " " + tag;
+	else if(mode == "exclude") query += " -" + tag;
+	else                       query = tag;
+	search_input.value = query;
+	search_button.click();
+}
