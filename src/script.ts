0f92fd4b5009a6a98e5d95a3d85766a767711f17
Rin Takanashi <dareka826@gmail.com>
2021-03-12 19:18:40 +0100

    Fixed lint errors in src/script.ts

--- src/script.ts
+++ src/script.ts
@@ -1,25 +1,25 @@
 // Get elem by id function
-function gid(id:string):any { return document.getElementById(id); }
+function gid(id:string):HTMLElement { return document.getElementById(id); }
 
 // Get arguments from GET request
-let parameters = new URLSearchParams(window.location.search);
+const parameters = new URLSearchParams(window.location.search);
 let query = parameters.get("q") || "";
 
 // Get the DOM elements
-let search_input:HTMLInputElement   = gid("search_input");
-let search_button:HTMLButtonElement = gid("search_button");
+const search_input = <HTMLInputElement>gid("search_input");
+const search_button = <HTMLButtonElement>gid("search_button");
 
 // Set the search field to the query
 search_input.value = query;
 
 // Reload with a new query
-function search_execute(page:number = 0) {
-	let parameters = new URLSearchParams();
+function search_execute(page=0) {
+	const parameters = new URLSearchParams();
 	parameters.set("q", search_input.value); // Set the query
 	parameters.set("p", page.toString());
 
 	// Create an <a> tag and use it to change location, preserving history
-	let elem_a = document.createElement("a");
+	const elem_a = document.createElement("a");
 	document.body.appendChild(elem_a);
 	elem_a.style.display = "none";
 	elem_a.href = "/?" + parameters.toString();
@@ -35,6 +35,8 @@ search_input.addEventListener("keypress", function(e) {
 
 // Changes the query by adding or excluding tags
 type TagChangeMode = "add" | "exclude";
+// The function is used in index.html, not here
+/* eslint-disable @typescript-eslint/no-unused-vars */
 function changeQuery(tag:string, mode:TagChangeMode) {
 	if(mode == "add")          query += " " + tag;
 	else if(mode == "exclude") query += " -" + tag;
