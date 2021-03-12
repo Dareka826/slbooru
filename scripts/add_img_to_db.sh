5bf29519aaa1b50cdd4968ebc4dda31dede9caaa
Rin Takanashi <dareka826@gmail.com>
2021-03-12 09:01:36 +0100

    Addition script: make extension lowercase, jpeg -> jpg

--- scripts/add_img_to_db.sh
+++ scripts/add_img_to_db.sh
@@ -8,8 +8,9 @@ for item in "$@"; do
 	# Calculate the item's checksum and get it's extension
 	CHKSUM=$(sha256sum "$item" | cut -d' ' -f1)
 	EXTENSION=$(echo "$item" | grep -Po "\.[^.]+$")
+	EXT_LC=$(echo "$EXTENSION" | tr "[:upper:]" "[:lower:]")
 
-	if [ "$EXTENSION" = ".mkv" ]; then
+	if [ "$EXT_LC" = ".mkv" ]; then
 		NEW_EXT=".mp4"
 		if ffprobe 2>&1 | grep Stream | grep Video | grep vp ; then
 			NEW_EXT=".webm"
@@ -17,20 +18,27 @@ for item in "$@"; do
 		ffmpeg -i "$item" -c copy "$TMPD/$(basename "$item")${NEW_EXT}"
 		item="$TMPD/$(basename "${item}${NEW_EXT}")"
 		CHKSUM=$(sha256sum "$item" | cut -d' ' -f1)
-		EXTENSION=$(echo "$item" | grep -Po "\.[^.]+$")
+		EXTENSION=$NEW_EXT
+		EXT_LC=$EXTENSION
+	fi
+	if [ "$EXT_LC" = ".jpeg" ]; then
+		cp "$item" "$TMPD/$(basename "$item").jpg"
+		item="$TMPD/$(basename "$item").jpg"
+		EXTENSION=".jpg"
+		EXT_LC=$EXTENSION
 	fi
 
 	# Check if same checksum exists in img/
-	if [ -e "metadata/${CHKSUM}${EXTENSION}" ]; then
+	if [ -e "metadata/${CHKSUM}${EXT_LC}" ]; then
 		echo "$item already exists in the db"
 	else
 		# If not, copy the file into img/
-		cp "$item" "img/${CHKSUM}${EXTENSION}"
+		cp "$item" "img/${CHKSUM}${EXT_LC}"
 		# Get the biggest exisitng id from metadata files
 		BIGGEST_ID=$(ls -1 metadata | sed 's/\.json//' | sort -r -n | head -1)
 		[ "$BIGGEST_ID" = "" ] && BIGGEST_ID=-1
 		# Create metadata file with empty tags
-		echo "{ \"file\": \"${CHKSUM}${EXTENSION}\", \"tags\": [] }" \
+		echo "{ \"file\": \"${CHKSUM}${EXT_LC}\", \"tags\": [] }" \
 			| jq > "metadata/$((BIGGEST_ID + 1)).json"
 		echo "Created metadata/$((BIGGEST_ID + 1)).json with empty tags"
 	fi
