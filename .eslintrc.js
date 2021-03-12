d4864407a96011944c068766c6b534e220ddb701
Rin Takanashi <dareka826@gmail.com>
2021-03-12 22:40:33 +0100

    Fix spacing in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-	parserOptions: {
+  parserOptions: {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json']
   },
