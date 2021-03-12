c254e9731ece85f3e78013bebdd2f847269863a2
Rin Takanashi <dareka826@gmail.com>
2021-03-12 06:44:02 +0100

    Fixed double dot in added filename

--- scripts/add_img_to_db.sh
+++ scripts/add_img_to_db.sh
@@ -9,16 +9,16 @@ for item in "$@"; do
 	EXTENSION=$(echo "$item" | grep -Po "\.[^.]+$")
 
 	# Check if same checksum exists in img/
-	if [ -e "metadata/${CHKSUM}.${EXTENSION}" ]; then
+	if [ -e "metadata/${CHKSUM}${EXTENSION}" ]; then
 		echo "$item already exists in the db"
 	else
 		# If not, copy the file into img/
-		cp "$item" "img/${CHKSUM}.${EXTENSION}"
+		cp "$item" "img/${CHKSUM}${EXTENSION}"
 		# Get the biggest exisitng id from metadata files
 		BIGGEST_ID=$(ls -1 metadata | sed 's/\.json//' | sort -r -n | head -1)
 		[ "$BIGGEST_ID" = "" ] && BIGGEST_ID=-1
 		# Create metadata file with empty tags
-		echo "{ \"file\": \"${CHKSUM}.${EXTENSION}\", \"tags\": [] }" \
+		echo "{ \"file\": \"${CHKSUM}${EXTENSION}\", \"tags\": [] }" \
 			| jq > "metadata/$((BIGGEST_ID + 1)).json"
 		echo "Created metadata/$((BIGGEST_ID + 1)).json with empty tags"
 	fi
