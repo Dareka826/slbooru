08619bed81ff22a077883a1970589e5e809fff1e
Rin Takanashi <dareka826@gmail.com>
2021-03-13 14:58:35 +0100

    Fix styling: tab indentation, space in single-line arrays

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: [ "./tsconfig.json" ]
-  },
-  plugins: [ "@typescript-eslint" ],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking"
-  ],
-  rules: {
-    "semi": [ "error", "always" ],
-    "space-before-function-paren": [ "warn", "never" ]
-  }
+	root: true,
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: [ "./tsconfig.json" ]
+	},
+	plugins: [ "@typescript-eslint" ],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/recommended-requiring-type-checking"
+	],
+	rules: {
+		"semi": [ "error", "always" ],
+		"space-before-function-paren": [ "warn", "never" ]
+	}
 };
